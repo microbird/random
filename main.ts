7609767dcf7e105c63582895d8f4f2f787e3b884
microbird <microbird@sina.com>
2023-04-28 14:56:28 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(0, top))
+    top += -1
+})
+let top = 0
+basic.showIcon(IconNames.Heart)
+top = 10
+basic.forever(function () {
+	
+})
